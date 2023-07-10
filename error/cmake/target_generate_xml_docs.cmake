13fc4f21b5ed799d73ce40cc86936a432f804ba4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-10 12:16:26 +0700

    build: use file glob to trigger XML docs generation

--- error/cmake/target_generate_xml_docs.cmake
+++ error/cmake/target_generate_xml_docs.cmake
@@ -10,11 +10,20 @@ function(target_generate_xml_docs TARGET)
     # Get include directories of the target
     foreach(PROP INCLUDE_DIRECTORIES INTERFACE_INCLUDE_DIRECTORIES)
       get_target_property(TARGET_INCLUDE_DIRS ${TARGET} ${PROP})
-      list(APPEND INCLUDE_DIRS ${TARGET_INCLUDE_DIRS})
+      if(NOT "${TARGET_INCLUDE_DIRS}" STREQUAL "TARGET_INCLUDE_DIRS-NOTFOUND")
+        list(APPEND INCLUDE_DIRS ${TARGET_INCLUDE_DIRS})
+      endif()
+    endforeach()
+    list(REMOVE_DUPLICATES INCLUDE_DIRS)
+
+    # Get header files from the include directories
+    foreach(DIR ${TARGET_INCLUDE_DIRS})
+      file(GLOB_RECURSE FILES CONFIGURE_DEPENDS "${DIR}/*")
+      list(APPEND HEADER_FILES ${FILES})
     endforeach()
 
-    # Generate XML documentation from the target
-    doxygen_add_docs(${TARGET}_docs ${INCLUDE_DIRS})
+    # Generate XML documentation for the target
+    doxygen_add_docs(${TARGET}_docs ${HEADER_FILES} USE_STAMP_FILE)
     add_dependencies(${TARGET} ${TARGET}_docs)
   else()
     message(WARNING "Could not found Doxygen!")
