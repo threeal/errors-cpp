ad222fa58ce8c71fbeec229b1db92f37cdf1d644
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-10 13:41:46 +0700

    docs: use Breathe to integrate Doxygen docs into Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -4,5 +4,13 @@ project = 'cpp'
 copyright = '2023, Alfi Maulana'
 author = 'Alfi Maulana'
 
+extensions = ['breathe']
+
+subprocess.call('cmake ../error -B ../error/build -D BUILD_DOCS=ON', shell=True)
+subprocess.call('cmake --build ../error/build --target docs', shell=True)
+
+breathe_projects = {"error": "../error/build/docs"}
+breathe_default_project = "error"
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
