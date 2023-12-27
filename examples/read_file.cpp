d6266b670ef7102c90980ea198b71759632f2f40
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-27 13:37:43 +0700

    feat(example): use `errors::format` in `read_file` example

--- examples/read_file.cpp
+++ examples/read_file.cpp
@@ -1,16 +1,16 @@
 #include <errors/error.hpp>
+#include <errors/format.hpp>
 #include <fstream>
-#include <iostream>
 
 errors::Error read_file(const char* filepath) {
   std::ifstream file(filepath);
   if (!file.is_open()) {
-    return errors::make("failed to open file");
+    return errors::format("failed to open `{}` ({})", filepath, file.rdstate());
   }
 
   std::string line;
   while (std::getline(file, line)) {
-    std::cout << line << std::endl;
+    fmt::print("{}\n", line);
   }
 
   return errors::nil();
@@ -18,13 +18,13 @@ errors::Error read_file(const char* filepath) {
 
 int main(int argc, char **argv) {
   if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " <filepath>" << std::endl;
+    fmt::print(stderr, "usage: {} <filepath>\n", argv[0]);
     return 1;
   }
 
   const auto err = read_file(argv[1]);
   if (err) {
-    std::cerr << err << std::endl;
+    fmt::print(stderr, "{}\n", err);
     return 1;
   }
 
