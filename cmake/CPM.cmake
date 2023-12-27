c178524dcef311ecd9ca66fcdc93ac70fba216a4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-27 22:13:41 +0700

    build: simplify `CPM.cmake` content

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -1,22 +1,7 @@
-# SPDX-License-Identifier: MIT
-#
-# SPDX-FileCopyrightText: Copyright (c) 2019-2023 Lars Melchior and contributors
-
-set(CPM_DOWNLOAD_VERSION 0.38.7)
-set(CPM_HASH_SUM 14ea07dfb484cad5db4ee1c75fd6a911)
-
-if(CPM_SOURCE_CACHE)
-  set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
-elseif(DEFINED ENV{CPM_SOURCE_CACHE})
-  set(CPM_DOWNLOAD_LOCATION "$ENV{CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
-else()
-  set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
-endif()
-
-# Expand relative path. This is important if the provided path contains a tilde (~)
-get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
-
-file(DOWNLOAD https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
-     ${CPM_DOWNLOAD_LOCATION} EXPECTED_MD5 ${CPM_HASH_SUM})
-
-include(${CPM_DOWNLOAD_LOCATION})
+file(
+  DOWNLOAD
+  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.38.7/CPM.cmake
+  ${CMAKE_BINARY_DIR}/_deps/CPM.cmake
+  EXPECTED_MD5 14ea07dfb484cad5db4ee1c75fd6a911
+)
+include(${CMAKE_BINARY_DIR}/_deps/CPM.cmake)
