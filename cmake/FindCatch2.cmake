12edfa98a9903a68cc355bae38ad86047ea78219
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-01 00:13:05 +0700

    build: add `FindCatch2.cmake` module
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ cmake/FindCatch2.cmake
@@ -0,0 +1,6 @@
+cpmaddpackage(gh:catchorg/Catch2@${Catch2_FIND_VERSION})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Catch2 REQUIRED_VARS Catch2_ADDED)
+
+list(PREPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
