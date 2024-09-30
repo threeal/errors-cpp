97b198530a78d96d005e97d6e81015b3bc75b7d6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-01 00:13:16 +0700

    build: add `FindCheckWarning.cmake` module
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ cmake/FindCheckWarning.cmake
@@ -0,0 +1,6 @@
+cpmaddpackage(gh:threeal/CheckWarning.cmake@${CheckWarning_FIND_VERSION})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CheckWarning REQUIRED_VARS CheckWarning.cmake_ADDED)
+
+list(PREPEND CMAKE_MODULE_PATH ${CheckWarning_SOURCE_DIR}/cmake)
