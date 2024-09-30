654c05d5738530b5dac636f2337314390ffa9358
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-01 00:13:23 +0700

    build: add `FindFixFormat.cmake` module
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ cmake/FindFixFormat.cmake
@@ -0,0 +1,6 @@
+cpmaddpackage(gh:threeal/FixFormat.cmake@${FixFormat_FIND_VERSION})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(FixFormat REQUIRED_VARS FixFormat.cmake_ADDED)
+
+list(PREPEND CMAKE_MODULE_PATH ${FixFormat_SOURCE_DIR}/cmake)
