0f001cd6e911fce8af312c93269ab77e57ef9541
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-17 16:35:13 +0700

    docs: bump copyright year to 2025 (#225)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import os, subprocess
 
 project = 'Errors C++'
 author = 'Alfi Maulana'
-copyright = '2023-2024, Alfi Maulana'
+copyright = '2023-2025, Alfi Maulana'
 version = '1.0.0'
 release = '1.0.0'
 
